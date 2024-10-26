ab6fd5145793b00ef32d74794044d6a84f64acf2
Electro-Corp <chinmaytanmay@outlook.com>
2024-10-26 13:04:22 -0600

    add telementry to new auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmodes/Auto.java
@@ -53,13 +53,13 @@ public class Auto extends LinearOpMode {
 
 
         while (!isStarted()) {
-
-
-
+            telemetry.addLine("Waiting to launch...");
+            telemetry.update();
         }
         while(!isStopRequested()) {
             switch (autoState) {
                 case GO_TO_TANK:
+                    telemetry.addLine("==== GO TO TANK ====");
                     roadRunnerHelper.forward(RoadRunnerHelper.TILE_SIZE_IN * 1.5)
                             .strafeLeft(RoadRunnerHelper.TILE_SIZE_IN)
                             .forward(RoadRunnerHelper.TILE_SIZE_IN)
@@ -69,42 +69,60 @@ public class Auto extends LinearOpMode {
                     autoState = AutoState.SEEK;
                     break;
                 case SEEK:
+                    telemetry.addLine("==== CHOOSE PIECE TO LOCK ON TO ====");
                      posX = autoPipeLine.getX();
                      posY = autoPipeLine.getY();
                      autoPipeLine.lockOnPoint(new Point(posX, posY));
                      autoState = AutoState.ALIGN_X;
                     break;
                 case ALIGN_X:
+                    telemetry.addLine("==== ALIGN ON X ====");
                     Point newLock = autoPipeLine.getClosestToLockOn();
+                    telemetry.addLine("--- LOCK ON POS: ");
+                    telemetry.addData("POS X", newLock.x);
+                    telemetry.addData("POS X", newLock.y);
                     Point alX = new Point(newLock.x , 0);
                     double dist = AutoPipeLine.getDist(alX, new Point(TARGET_X, 0));
                     if(dist < 10.0){
                         autoState = AutoState.ALIGN_Y;
                     }else{
                         if(TARGET_X > alX.x){
+                            telemetry.addLine("Target is greater than our pos, so we're");
+                            telemetry.addLine("going to [Strafe Left]");
                             roadRunnerHelper.strafeLeft(0.5);
                         }else if(TARGET_X < alX.x){
+                            telemetry.addLine("Target is less than our pos, so we're");
+                            telemetry.addLine("going to [Strafe Right]");
                             roadRunnerHelper.strafeLeft(-0.5);
                         }
                         autoPipeLine.lockOnPoint(newLock);
                     }
                     break;
                 case ALIGN_Y:
+                    telemetry.addLine("==== ALIGN ON Y ====");
                     Point newLockY = autoPipeLine.getClosestToLockOn();
+                    telemetry.addLine("--- LOCK ON POS: ");
+                    telemetry.addData("POS X", newLockY.x);
+                    telemetry.addData("POS X", newLockY.y);
                     Point alY = new Point(0 , newLockY.y);
                     double distY = AutoPipeLine.getDist(alY, new Point(0, TARGET_Y));
                     if(distY < 10.0){
                         autoState = AutoState.GRAB;
                     }else{
                         if(TARGET_X > alY.y){
+                            telemetry.addLine("Target is greater than our pos, so we're");
+                            telemetry.addLine("going to [Go Forward]");
                             roadRunnerHelper.forward(0.5);
                         }else if(TARGET_X < alY.y){
+                            telemetry.addLine("Target is less than our pos, so we're");
+                            telemetry.addLine("going to [Go Back]");
                             roadRunnerHelper.reverse(-0.5);
                         }
                         autoPipeLine.lockOnPoint(newLockY);
                     }
                     break;
                 case GRAB:
+                    telemetry.addLine("==== GRAB OBJECT ====");
                     arm.armAppendDist(ARM_DOWN_CONST);
                     claw.openClaw();
                     arm.pitchGoToGrab();
@@ -112,6 +130,7 @@ public class Auto extends LinearOpMode {
                     arm.pitchGoToPitchSeek();
                     break;
             }
+            telemetry.update();
         }
     }
 }
